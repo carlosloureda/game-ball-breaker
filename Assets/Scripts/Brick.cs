63a53cb1035af103dc6f023563efc7bb631dcd1b
Carlos Loureda <balanzeneto@gmail.com>
2019-06-20 05:09:46 +0200

    feat: Prefabs. Added 3 kind of bricks (1 hit, 2 hits and 3 hits)

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int maxHits;
+    private int timesHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timesHit = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        this.countBrickHits();
+        // this.checkForBrickDeath();
+    }
+    private void countBrickHits()
+    {
+        this.timesHit++;
+    }
+
+}
