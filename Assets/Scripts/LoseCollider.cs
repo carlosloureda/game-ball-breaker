6a58bcf3eab10847c7194f787b4cd42285c8378a
Carlos Loureda <balanzeneto@gmail.com>
2019-06-20 03:41:28 +0200

    First commit with ball bouncing on paddle

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+    public LevelManager levelManager;
+
+    private void OnCollisionEnter2D (Collision2D other) {
+        Debug.Log ("collision");
+    }
+    private void OnTriggerEnter2D (Collider2D other) {
+        Debug.Log ("trigger");
+        levelManager.LoadLevel ("GameOver");
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+    // Start is called before the first frame update
+    void Start () {
+        if (instance != null) {
+            Debug.Log ("Destroy music player instance");
+            Destroy (gameObject);
+        } else {
+            instance = this;
+            GameObject.DontDestroyOnLoad (gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
