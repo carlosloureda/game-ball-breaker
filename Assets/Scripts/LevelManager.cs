b1d1d1e477f220eb848489b90bdb27202fb7c297
Carlos Loureda <balanzeneto@gmail.com>
2019-06-20 07:44:44 +0200

    fix: Error with brickCount

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,7 @@ public class LevelManager : MonoBehaviour
 
     public void LoadLevel(string name)
     {
+        Brick.breakableCount = 0;
         SceneManager.LoadScene(name);
     }
     public void Quit()
@@ -17,6 +18,7 @@ public class LevelManager : MonoBehaviour
 
     public void LoadNextLevel()
     {
+        Brick.breakableCount = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
